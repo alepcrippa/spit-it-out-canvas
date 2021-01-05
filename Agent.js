48bd2a11e95c639a4bf6e45ffd3d2b9ae70c0e9b
chiara <chiara7.brambilla@mail.polimi.it>
2021-01-05 20:04:04 +0100

    prova

--- Agent.js
+++ Agent.js
@@ -6,7 +6,7 @@ class Agent{
   constructor(x0,y0){
     this.vector = myp5.createVector(x0, y0);//myp5.random(myp5.width), myp5.random(myp5.height));
     this.vectorOld = this.vector.copy();
-    this.stepSize = myp5.random(1, 5);
+    this.stepSize = myp5.random(1, 5);//lunghezza parola, sarebbe dasostituire con la lunghezza reale 
     this.isOutside = false;
     this.angle;
   }
--- sketch.js
+++ sketch.js
@@ -1,41 +1,12 @@
-// M_1_5_02
-//
-// Generative Gestaltung – Creative Coding im Web
-// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
-// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
-// with contributions by Joey Lee and Niels Poldervaart
-// Copyright 2018
-//
-// http://www.generative-gestaltung.de
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * noise values (noise 2d) are used to animate a bunch of agents.
- *
- * KEYS
- * 1-2                 : switch noise mode
- * space               : new noise seed
- * backspace           : clear screen
- * s                   : save png
- */
-
-
 var x = 0;
 var y = 0;
-var stepSize = 5.0;
+var stepSize = 5;
 
 //var font = 'Georgia';
 var letters = 'ciao'
 //'Così tra questa immensità s\'annega il pensier mio: e il naufragar m\'è dolce in questo mare.'
 //'All the world\'s a stage, and all the men and women merely players. They have their exits and their entrances.';
+
 var fontSizeMin = 3;
 var angleDistortion = 0.0;
 var counter = 0;
